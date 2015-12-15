dadf0b434312c8a322eb894ec25ad9f961abd28b
YHKaer <1019383943@qq.com>
2015-12-15 15:29:17 +0800

    first commint

---
+++ Rotary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rotary
+//
+//  Created by oscar on 15/12/15.
+//  Copyright © 2015年 oscar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rotary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotary
+//
+//  Created by oscar on 15/12/15.
+//  Copyright © 2015年 oscar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
